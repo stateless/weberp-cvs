cc4bfab4a59cb74905d1a2e49f0d0d2510d66ff2
Nicholas Lee <emptysands@gmail.com>
2009-06-02 20:13:23 +1200

    emdeex: Fix custcontacts default field types

---
+++ sql/mysql/weberp-3.10-004-customer.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `custcontacts` CHANGE `role` `role` VARCHAR( 40 ) NOT NULL;
+ALTER TABLE `custcontacts` CHANGE `phoneno` `phoneno` VARCHAR( 20 ) NOT NULL;
+ALTER TABLE `custcontacts` CHANGE `notes` `notes` VARCHAR( 255 ) NOT NULL;
